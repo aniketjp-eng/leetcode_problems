681f3f7d58b53a9769d82cd136fac00e4bd4322c
Aniket Prajapati <aniketjp.eng@gmail.com>
2026-05-28 21:13:01 +0530

    Fist Try not so optimal

---
+++ Rotate String/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+      public static boolean rotateString(String s, String goal) {
+        if(s.equals(goal)) {
+            return true;
+        }
+        StringBuilder sb = new StringBuilder(s);
+        for(int i = 0; i < sb.length()-1;i++){
+           char first = sb.charAt(0);
+           sb.deleteCharAt(0);
+           sb.append(first);
+           if(sb.toString().equals(goal)) {
+            return true;
+           }
+        }
+        return false;
+    }
+    public static void main(String [] args) {
+        // String s = "abcde";
+        // String goal = "cdeab";
+
+        // String s = "abcde";
+        // String goal = "abced";
+
+        String s = "abc";
+        String goal = "abc";
+      boolean ans = rotateString(s, goal);
+      System.out.println("Got the ans : "+ ans);
+    }
+}
