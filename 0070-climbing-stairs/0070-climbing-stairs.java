a5bccb0ae3bedfef7a526d037d901fb803cb4568
Aniket Prajapati <aniketjp.eng@gmail.com>
2026-06-25 21:59:41 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,10 @@
 class Solution {
   public static int fibo(int n, int[] dp) {
-        if (n == 0) {
-            return 0;
-        }
-        if (dp[n] != 0) {
-            return dp[n];
-        }
-        if (n == 1 || n == 2) {
-            return 1;
-        }
-        int ans = fibo(n - 1, dp) + fibo(n - 2, dp);
-        dp[n] = ans;
-        return ans;
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        if (dp[n] != 0) return dp[n];
+        dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
+        return dp[n];
     }
 
     public static int climbStairs(int n) {
